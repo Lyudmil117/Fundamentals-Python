cc29a11f6223241e71664e44eabdc695398d3a29
Lyudmil Nakashev <110760786+Lyudmil117@users.noreply.github.com>
2022-08-22 18:31:17 +0300

    Lists. Use MAP and function---->  ', '.join(list) ---how to remove the [] from the list when you print it

---
+++ lists_number_classification.py
@@ -0,0 +1,12 @@
+data = list(map(int, input().split(", ")))
+#this is how by MAP function we turn the input text from string to list with integers(now strings)
+
+positive_list = [str(x) for x in data if x >= 0] #X must be a string so later we can join it down
+negative_list = [str(x) for x in data if x < 0]
+even_list = [str(x) for x in data if x % 2 == 0]
+odd_list = [str(x) for x in data if x % 2 != 0]
+
+print(f"Positive: {', '.join(positive_list)}") #here!
+print(f'Negative: {", ".join(negative_list)}')# here we remove the [] because its wanted in the task
+print(f'Even: {", ".join(even_list)}')
+print(f'Odd: {", ".join(odd_list)}')
