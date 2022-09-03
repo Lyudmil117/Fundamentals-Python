5a899ee8447dd50081167674cb0dff5e36c36c42
Lyudmil Nakashev <110760786+Lyudmil117@users.noreply.github.com>
2022-09-03 18:13:40 +0300

    Add files via upload

---
+++ func_perfect_number.py
@@ -0,0 +1,14 @@
+def perf_number():
+    number = int(input())
+    sum = 1
+    for i in range(2, number):
+        if number % i == 0:
+            sum = sum + i
+            print(i)
+    if sum == number:
+        print('We have a perfect number!')
+    else:
+        print("It's not so perfect.")
+
+
+perf_number()
