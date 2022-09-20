77e6636ae8fc935197006ed3f73a6789baf3cf7e
Lyudmil Nakashev <110760786+Lyudmil117@users.noreply.github.com>
2022-09-20 14:49:41 +0300

    search for element in list/if found add it to second list(or remove it from first list and this way create second)

---
+++ F_5_4.py
@@ -0,0 +1,16 @@
+n = int(input())
+search_word = input()
+list1 = []
+list2 = []
+
+for _ in range(n):
+    word = input()
+    list1.append(word)
+print(list1)
+
+for i in range(len(list1) - 1, -1, -1):
+    element = list1[i]
+    if search_word not in element:
+        list1.remove(element)
+
+print(list1)
