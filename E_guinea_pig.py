e48b70b4fbc9babd9da94435b9ad352439e4edb8
Lyudmil Nakashev <110760786+Lyudmil117@users.noreply.github.com>
2022-10-05 10:50:46 +0300

    Add files via upload

---
+++ E_guinea_pig.py
@@ -0,0 +1,29 @@
+food = float(input()) * 1000
+hay = float(input()) * 1000
+cover = float(input()) * 1000
+pig_weight = float(input()) * 1000
+remain_food = 0
+
+
+for day in range(1, 30 + 1):
+    remain_food = food - 300
+    if remain_food < (food - 300):
+        break
+    if day % 2 == 0:
+        hay -= remain_food * 0.05
+        if hay < (remain_food * 0.05):
+            break
+
+    if day % 3 == 0:
+        cover -= pig_weight / 3
+        if cover < (pig_weight / 3):
+            break
+
+    food = remain_food
+
+
+if remain_food < 300 or hay < (remain_food * 0.95) or cover < (pig_weight / 3):
+    print("Merry must go to the pet store!")
+
+else:
+    print(f"Everything is fine! Puppy is happy! Food: {(remain_food / 1000):.2f}, Hay: {(hay / 1000):.2f}, Cover: {(cover / 1000):.2f}.")
