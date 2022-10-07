5ed3b1805b7e7e0e83f5e915035109773afd31bc
Lyudmil Nakashev <110760786+Lyudmil117@users.noreply.github.com>
2022-10-07 10:32:45 +0300

    Add files via upload

---
+++ F_8_4.py
@@ -0,0 +1,16 @@
+def odd_even():
+    num_list = []
+    odd_sum = 0
+    even_sum = 0
+    number = input()
+    for num in number:
+        num_list.append(int(num))
+    for x in num_list:
+        if x % 2 == 0:
+            even_sum += x
+        else:
+            odd_sum += x
+    print(f'Odd sum = {odd_sum}, Even sum = {even_sum}')
+
+
+odd_even()
