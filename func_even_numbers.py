d25d69332e1c81e819f90956eb51a7cfbfd534e6
Lyudmil Nakashev <110760786+Lyudmil117@users.noreply.github.com>
2022-10-07 10:45:25 +0300

    Update func_even_numbers.py

--- func_even_numbers.py
+++ func_even_numbers.py
@@ -1,6 +1,6 @@
 def even():
-    filtered = filter(lambda num: num % 2 == 0, list1) # here you you put function(condition), then the sequence(list in that case)
-    print(list(filtered))             #here you print it as a list
+    filtered = filter(lambda num: num % 2 == 0, list1)  # here you you put function(condition), then the sequence(list in that case)
+    print(list(filtered))                               #here you print it as a list
 
 
 list1 = input().split(' ')
