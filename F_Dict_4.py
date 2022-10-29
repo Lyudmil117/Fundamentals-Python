52be280f843734158291fd2220e7a701bd0b81ab
Lyudmil Nakashev <110760786+Lyudmil117@users.noreply.github.com>
2022-10-29 18:20:21 +0300

    Add files via upload

---
+++ F_Dict_4.py
@@ -0,0 +1,23 @@
+text = input()
+courses = {}
+
+while ":" in text:
+    data = text.split(":")
+    name = data[0]
+    id = data[1]
+    course = data[2]
+
+    if course not in courses.keys():  #може и без keys/ тук проверчвам дали го има в курсовете
+                                      #ако го няма го създавам
+        courses[course] = {}          # така създавам ГОЛЕМИЯ КУРС, а другите ще бъдат вложени
+
+    courses[course][id] = name      # РЕЧНИК = { КУРС : {ID (КЛЮЧ) : ИМЕ}} NESTED DICTIONARY
+
+    text = input()
+
+
+text = text.replace("_", " ")     #така на като получа WORD_SOMETHING , ЩЕ СТАНЕ WORD SOMETHING
+                                  # СТАВА И С: COURSE = " ".JOIN(TEXT.SPLIT("_") ---така ще заменя подчертавката с празно място
+
+for id in courses[text]:
+    print(f'{courses[text][id]} - {id}')
