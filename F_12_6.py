b8d3ce50ac241b554df7bfbfdd00acaf2b2b7518
Lyudmil Nakashev <110760786+Lyudmil117@users.noreply.github.com>
2022-11-16 20:49:42 +0200

    Update F_12_6.py

--- F_12_6.py
+++ F_12_6.py
@@ -5,3 +5,14 @@ for char in text:
         new_text += char
 
 print(new_text)
+
+
+# da nqma poredni povtoreniq, zatova taka se rewava
+# some_text = input()
+# final_text = ""
+# last_letter = ""
+# for current_letter in some_text:
+#     if current_letter != last_letter:
+#         final_text += current_letter
+#         last_letter = current_letter
+# print(final_text)
