64d8ce2ec4d6f34bf0c5af5f721844243308d72e
Lyudmil Nakashev <110760786+Lyudmil117@users.noreply.github.com>
2022-12-01 13:01:04 +0200

    Update FinalExam_RE_4.py

--- FinalExam_RE_4.py
+++ FinalExam_RE_4.py
@@ -8,7 +8,7 @@ for _ in range(num):
     text = input()
     match = re.findall(pattern, text)
     if match:
-        word = match[0][1]
+        word = match[0][1]    # така достъпвам 0-група, 2-рото нещо в нея-ДУМАТА
         for i in word:
             if i.isdigit():
                 product_group += i
@@ -19,4 +19,31 @@ for _ in range(num):
 
     else:
         print("Invalid barcode")
+
+# ОЩЕ ЕДИН НАЧИН
+
+import re
+n = int(input())
+pattern = r'@#{1,}([A-Z][a-zA-Z0-9]{4,}[A-Z])@#{1,}'
+
+for _ in range(n):
+    text = input()
+    list1 = []
+    result = re.findall(pattern, text)
+    for item in result:
+        list1.append(item)
+    if len(list1) == 0:
+        print("Invalid barcode")
+    else:
+        numbers = ""
+        for char in list1:
+            word = "".join(list1)
+            for i in word:
+
+                if i.isdigit():
+                    numbers += i
+            if len(numbers) == 0:
+                print("Product group: 00")
+            else:
+                print(f'Product group: {numbers}')
 
