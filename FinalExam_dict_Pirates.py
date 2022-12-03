2e213905fc3f682684fa88645ab2b1f1c7b18632
Lyudmil Nakashev <110760786+Lyudmil117@users.noreply.github.com>
2022-12-03 18:26:47 +0200

    Update FinalExam_dict_Pirates.py

--- FinalExam_dict_Pirates.py
+++ FinalExam_dict_Pirates.py
@@ -1,61 +1,47 @@
-cities_to_attack = {}
-
 command = input()
+cities = {}
 while command != "Sail":
     data = command.split("||")
-
     city = data[0]
     population = int(data[1])
     gold = int(data[2])
-
-    if city in cities_to_attack:
-        cities_to_attack[city][1] += gold
-        cities_to_attack[city][0] += population
+    if city in cities:
+        cities[city]['population'] += population
+        cities[city]['gold'] += gold
     else:
-        cities_to_attack[city] = city
-        cities_to_attack[city] = [population, gold]
-
+        cities[city] = {'population': population, 'gold': gold}
     command = input()
 
 command = input()
-
-while command != "End":
-    data2 = command.split("=>")
-    action = data2[0]
-    city = data2[1]
-
+while command != 'End':
+    data = command.split("=>")
+    action = data[0]
     if action == "Plunder":
-        people_killed = int(data2[2])
-        gold_to_plunder = int(data2[3])
+        town = data[1]
+        population_to_kill = int(data[2])
+        gold_to_steal = int(data[3])
+        cities[town]['population'] -= population_to_kill
+        cities[town]['gold'] -= gold_to_steal
 
-        if cities_to_attack[city][0] <= 0 or cities_to_attack[city][1] <= 0:
-            cities_to_attack.pop(city)
-            print(f"{city} has been wiped off the map!")
+        print(f"{town} plundered! {gold_to_steal} gold stolen, {population_to_kill} citizens killed.")
 
-        else:
-            cities_to_attack[city][0] -= people_killed
-            cities_to_attack[city][1] -= gold_to_plunder
-
-            if cities_to_attack[city][0] > 0 and cities_to_attack[city][1] > 0:
-                print(f"{city} plundered! {gold_to_plunder} gold stolen, {people_killed} citizens killed.")
-            else:
-
-                print(f"{city} plundered! {gold_to_plunder} gold stolen, {people_killed} citizens killed.")
-                print(f"{city} has been wiped off the map!")
-                cities_to_attack.pop(city)
+        if cities[town]['population'] <= 0 or cities[town]['gold'] <= 0:
+            print(f'{town} has been wiped off the map!')
+            cities.pop(town)
 
     elif action == "Prosper":
-        gold_to_add = int(data2[2])
-
+        town = data[1]
+        gold_to_add = int(data[2])
         if gold_to_add < 0:
-            print('Gold added cannot be a negative number!')
-            pass
+            print("Gold added cannot be a negative number!")
         else:
-            cities_to_attack[city][1] += gold_to_add
-            print(f'{gold_to_add} gold added to the city treasury. {city} now has {cities_to_attack[city][1]} gold.')
-
+            cities[town]['gold'] += gold_to_add
+            print(f"{gold_to_add} gold added to the city treasury. {town} now has {cities[town]['gold']} gold.")
     command = input()
 
-print(f"Ahoy, Captain! There are {len(cities_to_attack)} wealthy settlements to go to:")
-for town in cities_to_attack:
-    print(f'{town} -> Population: {cities_to_attack[town][0]} citizens, Gold: {cities_to_attack[town][1]} kg')
+if len(cities) > 0:
+    print(f'Ahoy, Captain! There are {len(cities)} wealthy settlements to go to:')
+    for town in cities:
+        print(f"{town} -> Population: {cities[town]['population']} citizens, Gold: {cities[town]['gold']} kg")
+else:
+    print("Ahoy, Captain! All targets have been plundered and destroyed!")
