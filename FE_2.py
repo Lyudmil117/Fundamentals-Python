e684619ca262be16c6b2aeb1442dde6e05424866
Lyudmil Nakashev <110760786+Lyudmil117@users.noreply.github.com>
2022-12-04 17:34:49 +0200

    Add files via upload

---
+++ FE_2.py
@@ -0,0 +1,18 @@
+import re
+n = int(input())
+
+pattern = r'!([A-Z][a-z]{2,})!:\[([A-Za-z]{8,})\]'
+
+for _ in range(n):
+    text = input()
+    result = re.findall(pattern, text)
+    if len(result) == 0:
+        print("The message is invalid")
+    else:
+        lst = []
+        for char in result[0][1]:
+            lst.append(ord(char))
+        clean = ""
+        for i in lst:
+            clean += " " + str(i)
+        print(f'{result[0][0]}:{clean}')
